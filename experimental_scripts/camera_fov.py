3b76beff30eb406999949801675902f5ef220f4d
Benjamin Humpherys <mooglinux@gmail.com>
2018-11-14 19:26:04 -0700

    Updated camera_fov.py to correctly update colors. Still does not handle occluded geometry.

--- experimental_scripts/camera_fov.py
+++ experimental_scripts/camera_fov.py
@@ -10,12 +10,31 @@ me = obj.data
 mesh = bmesh.from_edit_mesh(me)
 mesh.faces.active = None
 
+color_layer = mesh.loops.layers.color['Col']
+
 cam = bpy.data.objects["Camera"]
 
+
+# NOTE: This does not yet correctly handle occluded geometry!
 for v in mesh.verts:
+    color = (1, 1, 1)
+
     p = world_to_camera_view(bpy.context.scene, cam, v.co)
+    # Check if vertex is visible to camera
     if (0 < p.x < 1) and (0 < p.y < 1):
-        v.select = True
+        # v.select = True
+        # Check faces connected to vertex
+        for face in v.link_faces:
+            direction = face.normal - cam.location
+
+            if direction.dot(face.normal) < 0:
+                face.select = True
+                color = (1, 0, 1)
+
+    for face in v.link_faces:
+        # Color faces depending on dot product direction
+        for loop in face.loops:
+            loop[color_layer] = color
 
 # Write out changes
 bmesh.update_edit_mesh(me, True)
