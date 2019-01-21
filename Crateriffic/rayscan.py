3803f76f283afefaa26624b0b30d8722907030d1
Benjamin Humpherys <mooglinux@gmail.com>
2019-01-21 15:02:19 -0700

    Raycast now draws points in 3D.

--- Crateriffic/rayscan.py
+++ Crateriffic/rayscan.py
@@ -1,5 +1,7 @@
 import bpy
+import bgl
 import bmesh
+from bpy_extras.view3d_utils import location_3d_to_region_2d
 from mathutils import Vector
 from mathutils.bvhtree import BVHTree
 
@@ -11,59 +13,81 @@ class RayScanOperator(bpy.types.Operator):
     count_x = bpy.props.IntProperty(name="X Samples", min=1, default=10)
     count_y = bpy.props.IntProperty(name="Y Samples", min=1, default=10)
 
+    points = []
 
     @classmethod
     def poll(cls, context):
         return context.object.type == 'MESH'
-        # return True
 
 
     def invoke(self, context, event):
-        # TODO: Change this to be the currently active mesh
-        cube = bpy.data.objects['Cube']
-        self.bvh = BVHTree.FromObject(cube, bpy.context.scene)
+        if context.object.type != 'MESH':
+            self.report({'ERROR_INVALID_CONTEXT'}, "Must select Mesh object")
+            return {'CANCALLED'}
+
+        args = (self, context)
+        self._handle = bpy.types.SpaceView3D.draw_handler_add(self.draw_handler, args, 'WINDOW', 'POST_PIXEL')
+
         context.window_manager.modal_handler_add(self)
-        return {'RUNNING_MODAL'}
+        # return {'RUNNING_MODAL'}
+        return self.execute(context)
 
     
     def modal(self, context, event):
-        v3d = context.space_data
-        rv3d = v3d.region_3d
+        context.area.header_text_set("Running modal")
 
-        if event.type == 'MOUSEMOVE':
-            self.execute(context)
-            context.area.header_text_set("Running modal")
-
-        elif event.type == 'LEFTMOUSE':
+        context.area.tag_redraw()
+        
+        if event.type == 'LEFTMOUSE':
             context.area.header_text_set()
+            bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
             return {'FINISHED'}
 
         elif event.type in {'RIGHTMOUSE', 'ESC'}:
             context.area.header_text_set()
+            bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
             return {'CANCELLED'}
+        
+        else:
+            return {'PASS_THROUGH'}
 
         return {'RUNNING_MODAL'}
 
 
+    def draw_handler(self, context, args):
+        # Draw stuff here
+        bgl.glEnable(bgl.GL_BLEND)
+        bgl.glColor4f(1.0, 0.0, 1.0, 1.0)
+        bgl.glPointSize(4)
+
+        bgl.glBegin(bgl.GL_POINTS)
+        for point in self.points:
+            x, y = location_3d_to_region_2d(bpy.context.region, bpy.context.space_data.region_3d, point)
+            bgl.glVertex2f(x, y)
+
+        bgl.glEnd()
+
+        # restore opengl defaults
+        bgl.glPointSize(1)
+        bgl.glDisable(bgl.GL_BLEND)
+        bgl.glColor4f(0.0, 0.0, 0.0, 1.0)
+
     def execute(self, context):
-        bvh = self.bvh
+        bvh = BVHTree.FromObject(context.object, context.scene)
 
         # Get object data
-        # TODO: Make these into properties stored extrernal to the operator
-        sphere = bpy.data.objects['sphere']
+        # TODO: Make these into properties stored external to the operator
         camera = bpy.data.objects['Camera']
 
         # Find out which direction the camera is pointing
         # TODO: Modify to cast a grid of vectors using camera.data.angle_x/y
-        cam_dir = Vector((0, 0, -1))
-        cam_dir.rotate(camera.rotation_euler)
+        ray_dir = Vector((0, 0, -1))
+        ray_dir.rotate(camera.rotation_euler)
 
         # Cast a ray towards the center of the camera view
-        loc, norm, index, distance = bvh.ray_cast(camera.location, cam_dir)
+        loc, norm, index, distance = bvh.ray_cast(camera.location, ray_dir)
         if loc is not None:
-            # Move sphere to the intersection
-            # TODO: Change this to drawing with bgl
-            sphere.location = loc
+            self.points.append(loc)
         print(loc)
 
         return {'RUNNING_MODAL'}
