4307a438ebef6a257a13b2386c6f71571c20a1f8
Benjamin Humpherys <mooglinux@gmail.com>
2019-01-30 15:17:26 -0700

    Fixes and refactoring of rayscan.

--- Crateriffic/rayscan.py
+++ Crateriffic/rayscan.py
@@ -6,6 +6,7 @@ from mathutils import Vector, Euler
 from mathutils.bvhtree import BVHTree
 from .utils import linspace
 
+
 class RayScanOperator(bpy.types.Operator):
     bl_idname = "mesh.rayscan"
     bl_label = "Ray Scan"
@@ -15,6 +16,7 @@ class RayScanOperator(bpy.types.Operator):
     count_y = bpy.props.IntProperty(name="Y Samples", min=1, default=10)
 
     points = []
+
 
     @classmethod
     def poll(cls, context):
@@ -30,7 +32,6 @@ class RayScanOperator(bpy.types.Operator):
         self._handle = bpy.types.SpaceView3D.draw_handler_add(self.draw_handler, args, 'WINDOW', 'POST_PIXEL')
 
         context.window_manager.modal_handler_add(self)
-        # return {'RUNNING_MODAL'}
 
         area = next(area for area in bpy.context.screen.areas if area.type == 'VIEW_3D')
         space = next(space for space in area.spaces if space.type == 'VIEW_3D')
@@ -43,12 +44,11 @@ class RayScanOperator(bpy.types.Operator):
     
     def modal(self, context, event):
         context.area.header_text_set("Showing intersections. ESC to exit")
-
         context.area.tag_redraw()
-        area = next(area for area in bpy.context.screen.areas if area.type == 'VIEW_3D')
-        space = next(space for space in area.spaces if space.type == 'VIEW_3D')
 
         if event.type in {'ESC'}:
+            area = next(area for area in bpy.context.screen.areas if area.type == 'VIEW_3D')
+            space = next(space for space in area.spaces if space.type == 'VIEW_3D')
             context.area.header_text_set()
             bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
             space.show_only_render = self.old_show_render
@@ -65,10 +65,11 @@ class RayScanOperator(bpy.types.Operator):
     def draw_handler(self, context, args):
         # Draw stuff here
         bgl.glEnable(bgl.GL_BLEND)
-        bgl.glColor4f(1.0, 0.5, 1.0, 1.0)
-        bgl.glPointSize(8)
+        bgl.glColor4f(1.0, 0.74, 0.0, 1.0)
+        bgl.glPointSize(5)
 
         bgl.glBegin(bgl.GL_POINTS)
+
         for point in self.points:
             x, y = location_3d_to_region_2d(bpy.context.region, bpy.context.space_data.region_3d, point)
             bgl.glVertex2f(x, y)
@@ -79,6 +80,31 @@ class RayScanOperator(bpy.types.Operator):
         bgl.glPointSize(1)
         bgl.glDisable(bgl.GL_BLEND)
         bgl.glColor4f(0.0, 0.0, 0.0, 1.0)
+
+
+    def scan(self, bvh, angle_x, angle_y, count_x, count_y, camera_location, camera_rotation):
+        new_points = []
+
+        points_x = linspace(-angle_x/2, angle_x/2, self.count_x)
+        points_y = linspace(-angle_y/2, angle_y/2, self.count_y)
+
+        for py in points_y:
+            for px in points_x:
+                ray_dir = Vector((0, 0, -1))
+                # NOTE: From perspective of camera, rotating around x produces vertical tilt.
+                # BUG: Noticable distortion! Points may go outside FOV!
+                ray_dir.rotate(Euler((py, px, 0)))
+                ray_dir.rotate(camera_rotation)
+
+                # Cast a ray towards the center of the camera view
+                # BUG: Does not account for cases when the object has been rotated!!!!
+                #      May need to transform camera position and ray into object space. :/
+                loc, norm, index, distance = bvh.ray_cast(camera_location, ray_dir)
+                if loc is not None:
+                    new_points.append(loc)
+        
+        return new_points
+
 
     def execute(self, context):
         self.points.clear()
@@ -91,23 +117,8 @@ class RayScanOperator(bpy.types.Operator):
         # Find out which direction the camera is pointing
         angle_x = camera.data.angle_x
         angle_y = camera.data.angle_y
-
-        points_x = linspace(-angle_x/2, angle_x/2, self.count_x)
-        points_y = linspace(-angle_y/2, angle_y/2, self.count_y)
         
-        for py in points_y:
-            for px in points_x:
-                ray_dir = Vector((0, 0, -1))
-                # NOTE: From perspective of camera, rotating around x produces vertical tilt.
-                # BUG: Noticable distortion! Points may go outside FOV!
-                ray_dir.rotate(Euler((py, px, 0)))
-                ray_dir.rotate(camera.rotation_euler)
-
-                # Cast a ray towards the center of the camera view
-                # BUG: Does not account for cases when the object has been rotated!!!!
-                loc, norm, index, distance = bvh.ray_cast(camera.location, ray_dir)
-                if loc is not None:
-                    self.points.append(loc)
-        # print(loc)
+        self.points = self.scan(bvh, angle_x, angle_y, self.count_x, self.count_y, 
+                                camera.location, camera.rotation_euler)
 
         return {'RUNNING_MODAL'}
